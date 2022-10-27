e7fc6c4fc35ea1e3503e7a8b5981b7d0e98d74f1
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-10-27 21:30:08 +0600

    sidebar created

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,50 @@
-import { Box } from '@mui/material';
+import { AccountBoxOutlined, Article, Group, Home, Pages, Settings, Store } from '@mui/icons-material';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import React from 'react';
 
 const Sidebar = () => {
     return (
-        <Box flex={2} bgcolor='greenyellow' p={2} sx={{display:{xs:'none',sm:'none',md:'block'}}}>
-            sidebar
+        <Box flex={1} p={2} sx={{display:{xs:'none',sm:'none',md:'block'}}}>
+            <List>
+          <ListItem sx={{display:'flex',flexDirection:'column',alignItems:'start'}} disablePadding>
+            <ListItemButton component='a' href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+            <ListItemButton component='b' href="#page">
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+            <ListItemButton component='c' href="#Group">
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary="Group" />
+            </ListItemButton>
+            <ListItemButton component='d' href="#Store">
+              <ListItemIcon>
+                <Store />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+            <ListItemButton component='e' href="#profile">
+              <ListItemIcon>
+                <AccountBoxOutlined />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+            <ListItemButton component='f' href="#setting">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          </List>
         </Box>
     );
 };
