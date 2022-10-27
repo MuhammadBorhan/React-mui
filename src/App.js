040e2ef92a537e39c05f40320da33a6a2833fbb9
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-10-27 13:00:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="text">Text</Button>
+      <Button startIcon={<Settings />} variant="contained" color="secondary">
+        Setting
+      </Button>
+      <Button startIcon={<Add />} variant="contained" color="secondary">
+        Add
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+
+      <Typography variant="p" component="h1">
+        h1. Heading
+      </Typography>
+
+      <p>borhan</p>
+      <h1>boahan</h1>
+      <p>borhan</p>
+      <div></div>
+      <p></p>
+      <h1></h1>
     </div>
   );
 }
